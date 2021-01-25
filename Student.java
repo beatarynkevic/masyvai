0432b75a21ca2434fdbeff9c4a12355c46741481
Beata <62422202+beatarynkevic@users.noreply.github.com>
2021-01-25 13:02:57 +0200

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+package javaapplication37;
+
+public class Student {
+    
+    public int pozicijosNumeris;
+    public String studentoVardas;
+    
+    public Student(int skaicius, String vardas) {
+        this.pozicijosNumeris = skaicius;
+        this.studentoVardas = vardas;
+    }
+}
